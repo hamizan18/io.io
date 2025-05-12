0aea6788c6b798b15e70126f9ea6aa3c2171de3b
Hamizan Desu <hamizanputra321@gmil.com>
2025-05-13 02:15:38 +0700

    day-2

--- src/main.rs
+++ src/main.rs
@@ -1,24 +1,22 @@
+use std::io;
+
 fn main(){
 
-    let mut first = String::new();
-    println!("Masukkan nilai a: ");
-    let a = std::io::stdin().read_line(&mut first).unwrap();
-    println!("Hii, {}", first);
-    println!("Nilai a adalah: {}", a);
+    let mut input = String::new();
+
+    print!("Masukkan nilai a: ");
+    io::Write::flush(&mut io::stdout()).unwrap();
+    io::stdin().read_line(&mut input).unwrap();
+    let a : i32 = input.trim().parse().unwrap();
+
+    input.clear();
+
+    print!("Masukkan nilai b: ");
+    io::Write::flush(&mut io::stdout()).unwrap();
+    io::stdin().read_line(&mut input).unwrap();
+    let b : i32 = input.trim().parse().unwrap();
+
+    let res = a + b;
+    println!("Hasil dari {} + {} adalah: {}", a, b, res);
 
 }
-// fn main(){
-// let mut count = 1;
-
-//     let result = loop {
-//     println!("Hello!");
-
-//     if count < 3 {
-//         break count; // Stop the loop and return the number 3
-//     }
-
-//     count += 1;
-//     };
-
-// println!("The loop stopped at: {}", result);
-// }
