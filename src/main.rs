0a557bc82b38238caeaa9bdf1632fd7f8ba17012
Hamizan Desu <hamizanputra321@gmil.com>
2025-05-14 00:42:30 +0700

    day-3

--- src/main.rs
+++ src/main.rs
@@ -4,19 +4,34 @@ fn main(){
 
     let mut input = String::new();
 
-    print!("Masukkan nilai a: ");
+    print!("Masukkan nama anda: ");
     io::Write::flush(&mut io::stdout()).unwrap();
     io::stdin().read_line(&mut input).unwrap();
-    let a : i32 = input.trim().parse().unwrap();
-
+    let nama = input.trim().to_string();
     input.clear();
 
+    
+    print!("Masukkan umur anda sebelum memulai: ");
+    io::Write::flush(&mut io::stdout()).unwrap();
+    io::stdin().read_line(&mut input).unwrap();
+    let umur: u32 = input.trim().parse().unwrap();
+    
+    println!("\nNama anda adalah: {}", nama.trim());
+    println!("Anda berumur {}", umur);
+
+    input.clear();
+    print!("\nMasukkan angka pertama: ");
+    io::Write::flush(&mut io::stdout()).unwrap();
+    io::stdin().read_line(&mut input).unwrap();
+    let a: i32 = input.trim().parse().unwrap();
+
+    input.clear();
     print!("Masukkan nilai b: ");
     io::Write::flush(&mut io::stdout()).unwrap();
     io::stdin().read_line(&mut input).unwrap();
-    let b : i32 = input.trim().parse().unwrap();
+    let b: i32 = input.trim().parse().unwrap();
 
-    let res = a + b;
-    println!("Hasil dari {} + {} adalah: {}", a, b, res);
-
+    let result = a + b;
+    
+    println!("Hasil dari penjumlahan {} + {} adalah {}", a, b, result);
 }
